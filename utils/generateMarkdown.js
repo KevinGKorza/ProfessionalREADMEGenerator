c850c91bf0bbb5eb8f2fcbac6b2475be3c6c7163
Kevin Korza <107637260+KevinGKorza@users.noreply.github.com>
2022-08-30 22:43:06 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,7 @@
     -[Usage] (#usage)
     -[Contribution] (#contribution)
     -[Testing] (#testing)
-    -[Additional Information] (#additionalinformation)
+    -[Additional Information] (#additional Information)
   
     ## Description:
     ${data.description}
